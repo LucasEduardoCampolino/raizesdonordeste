9d77ba4b2a4980aa92dbedc175854ca7b6d36949
LucasEduardoCampolino <campolino.l@gmail.com>
2026-04-26 17:49:35 -0300

    Implementa bloqueio por role e adiciona um usuário admin ao subir sistema.

---
+++ src/main/java/com/raizesdonordeste/config/DataInitializer.java
@@ -0,0 +1,39 @@
+package com.raizesdonordeste.config;
+
+import com.raizesdonordeste.model.entity.Usuario;
+import com.raizesdonordeste.model.enums.PerfilEnum;
+import com.raizesdonordeste.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final UsuarioRepository repository;
+    private final PasswordEncoder encoder;
+
+    public DataInitializer(UsuarioRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        if (repository.findByEmail("admin@raizes.com").isEmpty()) {
+
+            Usuario admin = Usuario.builder()
+                    .nome("Administrador")
+                    .email("admin@raizes.com")
+                    .senha(encoder.encode("123456"))
+                    .perfil(PerfilEnum.ADMIN)
+                    .saldoPontos(0)
+                    .build();
+
+            repository.save(admin);
+
+            System.out.println("✅ ADMIN criado");
+        }
+    }
+}
--- src/main/java/com/raizesdonordeste/dto/UsuarioDTO.java
+++ src/main/java/com/raizesdonordeste/dto/UsuarioDTO.java
@@ -1,13 +1,10 @@
 package com.raizesdonordeste.dto;
 
-import com.raizesdonordeste.model.enums.PerfilEnum;
 import lombok.Data;
 
 @Data
 public class UsuarioDTO {
-
     private String nome;
     private String email;
     private String senha;
-    private PerfilEnum perfil;
 }
